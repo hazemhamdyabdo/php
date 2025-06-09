002e4426e7de4f1a5f6b45014828a4d8b34c8b62
hazem <hazemhamdy117@gmail.com>
2025-06-10 00:24:47 +0300

    feat: add note creation form with validation and database insertion

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,13 +1,29 @@
 <?php 
 $header =  'Create Note';
 $db = new Database($config['database']);
+$errors = [];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $payload = $_POST['body'];
     
-    $db->query("INSERT INTO notes (body,user_id) VALUES (:body,:user_id)", [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
+    
+
+    if (strlen($payload) === 0) {
+        $errors['body'] = 'Body is required';
+    } else if (!is_string($payload)) {
+        $errors['body'] = 'Body must be a string';
+    }else if (strlen($payload) < 10) {
+        $errors['body'] = 'Body must be at least 10 characters long';
+    } else if (strlen($payload) > 255) {
+        $errors['body'] = 'Body must be at most 255 characters long';
+    }
+    
+    if (empty($errors)) {
+        $db->query("INSERT INTO notes (body,user_id) VALUES (:body,:user_id)", [
+            'body' => $payload,
+            'user_id' => 1
+        ]);
+    }
 
     
 }
--- views/create-notes.view.php
+++ views/create-notes.view.php
@@ -5,15 +5,22 @@
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
 
-    <form method="POST">
+    <form method="POST" >
       <div class="col-span-full">
         <label for="body" class="block text-sm/6 font-medium text-gray-900">Body</label>
         <div class="mt-2">
-          <textarea name="body" id="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+          <textarea name="body" id="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+
+<?=  $_POST['body'] ?? '' ?>
+
+          </textarea>
+          <?php if (isset($errors['body'])): ?>
+            <p class="mt-2 text-sm text-red-600"><?= $errors['body'] ?></p>
+          <?php endif; ?>
         </div>
         <p class="mt-3 text-sm/6 text-gray-600"> Write a few sentences about yourself.</p>
       </div>
-      <button type="submit" class="mt-4 inline-flex items-center gap-2 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500">Submit</button>
+      <button  type="submit" class="mt-4 inline-flex items-center gap-2 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500">Submit</button>
     </form>
 </main>
 
